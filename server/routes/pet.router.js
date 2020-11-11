c7722ce4be83e20f91b26cf7631a224cd8c966f2
Kelly Danger <kellyanndanger@gmail.com>
2020-11-11 16:04:04 -0600

    lecture complete added auth

--- server/routes/pet.router.js
+++ server/routes/pet.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { eventChannel } = require('redux-saga');
 const router = express.Router();
 const pool = require('../modules/pool');
 
@@ -7,14 +8,22 @@ router.get('/', (req, res) => {
     console.log('/pet GET route');
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
-    let queryText = `SELECT * FROM "pet"`;
-    pool.query(queryText).then((result) => {
+
+    if(req.isAuthenticated() === false) {
+        res.sendStatus(403);
+    } else {
+        let queryText = `SELECT * FROM "pet" WHERE "user_id" = $1`;
+        pool.query(queryText, [req.user.id]).then((result) => {
         res.send(result.rows);
     }).catch((error) => {
         console.log(error);
         res.sendStatus(500);
     });
+    }
 });
+
+//could also use for admin stuff...
+// if(user.admin === true)...or if(user.level > 3) etc.
 
 // This route *should* add a pet for the logged in user
 router.post('/', (req, res) => {
@@ -22,8 +31,17 @@ router.post('/', (req, res) => {
     console.log(req.body);
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
-    res.sendStatus(200);
-    
+    if(req.isAuthenticated()){
+        let queryText = `INSERT INTO "pet" ("firstname", "user_id") VALUES ($1, $2);`;
+        pool.query(queryText, [req.body.firstName, req.user.id])
+        .then((result) => {
+            res.sendStatus(200);      
+        }).catch((error) => {
+            res.sendStatus(500)
+        })
+    } else {
+        res.sendStatus(403);
+    } 
 });
 
 module.exports = router;
